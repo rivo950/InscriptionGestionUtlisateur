3fdc3af5dc0d60e36401933af7f9b7256ab3160b
Rivo RAFIDIMANANA <rafidimananarivo@gmail.com>
2023-08-21 23:18:34 +0300

    Create AuthController and change route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('user', [AuthController::class, 'ndex']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 Route::get('/', function () {
     return view('welcome');
 });
